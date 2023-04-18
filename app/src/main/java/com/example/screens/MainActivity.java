6991c21969903237562567d9e5baaf222241c2b7
a22robko <a22robko@student.his.se>
2023-04-18 15:19:22 +0200

    Jag har skapat en button knapp som kopplas till andra sidan också.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
-
+import android.util.Log;
 public class MainActivity extends AppCompatActivity {
+    private Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b =(Button)findViewById(R.id.button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>","We clicked on Tomten!");
+                setContentView(R.layout.activity_button2);
+
+            }
+
+
+        });
     }
 }
